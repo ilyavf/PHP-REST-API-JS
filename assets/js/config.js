0744231c156d5560990efd5755e3ff96339ec6bc
Marc Godard <me@marcgodard.com>
2016-03-08 00:10:00 -0500

    Removed Unneeded Settings

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,31 +5,7 @@ define(function() {
       {name: "Français", code: "fr"}
     ],
     server: {
-      domain: "http://edc2.auto-cms.com"
-    },
-    barDefaults: {
-      barColors: ['#DA650E'],
-      resize: true
-    },
-    areaDefaults: {
-      fillOpacity: 0.5,
-      lineColors: ['#41A69B'],
-      pointFillColors:['#ffffff'],
-      pointStrokeColors: ['black'],
-      resize: true
-    },
-    lineDefaults: {
-      lineColors: ['#41A69B'],
-      pointFillColors:['#ffffff'],
-      pointStrokeColors: ['black'],
-      resize: true
-    },
-    donutDefaults: {
-      colors: ['#66D654','#0F7000','#23B40C','#44C82F','#159102'],
-      resize: true
-    },
-    dtDefaults: {
-      pageLength: 5
+      domain: "http://api.domain.com"
     }
   }
 });
